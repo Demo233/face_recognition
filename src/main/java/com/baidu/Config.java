bb2a3d004e8d37ece23030f77f125b6e72e0c4b7
Zhao Yihao <cnnqjban521@gmail.com>
2018-10-08 15:55:08 +0800

    add

--- src/main/java/com/baidu/Config.java
+++ src/main/java/com/baidu/Config.java
@@ -1,7 +1,7 @@
 package com.baidu;
 
 public class Config {
-	public final static String APPID = "14325504";
-	public final static String APIKEY = "iK0m7PTivPzvWA0gIDFe7Vbp";
-	public final static String SECRETKEY = "rVd64Cy3IUkZsWNYzubWQMRO0dGr0igS";
+	public final static String APPID = "143255041";
+	public final static String APIKEY = "iK0m7PTivPzvWA0gIDFe7Vbp1";
+	public final static String SECRETKEY = "rVd64Cy3IUkZsWNYzubWQMRO0dGr0igS1";
 }
